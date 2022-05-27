0de5b5b468b62ee1c0bfe1a8704b86c2b7337d67
nnyong <68502088+nny0ng@users.noreply.github.com>
2022-05-27 23:55:59 +0900

    0527 크롤링 문제 해결

--- new_preprocessing.py
+++ new_preprocessing.py
@@ -4,100 +4,114 @@ import json
 from transformers import ElectraForSequenceClassification, ElectraTokenizerFast
 
 api_key = 'AIzaSyC4z-yJBlW3uNziSyQxZ7hydDm6GhxMD7U'
-video_id = 'fU6lxdxTdU4'
+video_id = '9Ka1qtPAD-w'
 
 api_obj = build('youtube', 'v3', developerKey=api_key)
 response = api_obj.commentThreads().list(part="id, replies, snippet", videoId=video_id, maxResults=100).execute()
 
 cur_index = 0
-result = {}
+result = []
 
 while response:
+    ### datatype "0" 원댓글
     for item in response['items']:
         comment = item['snippet']['topLevelComment']['snippet']
-        ### datatype "0" 원댓글
+
         result_format = {
+            "index": cur_index,
             "datatype": "0",
             "toWho": "",
-            #"cid": comment['authorChannelId']['value'],
             "author": comment['authorDisplayName'],
-            "published_date": comment['publishedAt'],
-            "time_num": re.sub(r'[^0-9]', '', comment['publishedAt']),
+            "publishedDate": comment['publishedAt'],
+            "timeNum": re.sub(r'[^0-9]', '', comment['publishedAt']),
             "text": comment['textDisplay']
         }
-        result[cur_index] = result_format
+        result.append(result_format)
         cur_index = cur_index + 1
 
+        ### datatype "1", "2" 대댓글
         if 'replies' in item.keys():
             reply_num = 0
-            for reply in item['replies']['comments']:
-                reply['snippet']['textDisplay'] = reply['snippet']['textDisplay'].lstrip()
+            parent = item['id']
+            request = api_obj.comments().list(part='snippet', parentId=parent, maxResults=100).execute()
+            while request:
+                for reply in request['items']:
+                    reply['snippet']['textDisplay'] = reply['snippet']['textDisplay'].lstrip()
 
-                ### datatype "2" 언급대댓글
-                if reply['snippet']['textDisplay'][0] == '@':
-                    reference = reply['snippet']['textDisplay'].split(' ', 1)
-                    reference = reference[0].replace("@", "")
+                    ### datatype "2" 언급대댓글
+                    if reply['snippet']['textDisplay'][0] == '@':
+                        reference = reply['snippet']['textDisplay'].split(' ', 1)
+                        reference = reference[0].replace("@", "")
 
-                    close_reference_index = 0
-                    close_reference = []
-                    reference_toWho = 0
+                        close_reference_index = -1
+                        close_reference = []
+                        reference_toWho = 0
 
-                    ### 언급한 댓글 찾는 과정
-                    for re_reply in item['replies']['comments']:
-                        close_reference_index = close_reference_index + 1
-                        if re_reply['snippet']['authorDisplayName'] == reference:
-                            close_reference.append(close_reference_index)
+                        ### 언급한 댓글 찾는 과정
+                        for re_reply in request['items']:
+                            close_reference_index = close_reference_index + 1 # 대댓글 내 target 대댓글 index
+                            if re_reply['snippet']['authorDisplayName'] == reference: # 언급자와 target 대댓글 author가 같을 경우
+                                close_reference.append(close_reference_index) # close_reference에 target 대댓글 index 추가
 
-                    if len(close_reference) == 0:
-                        if reference == comment['authorDisplayName']:
-                            reference_toWho = cur_index - reply_num
+                        if len(close_reference) == 0: # 언급자와 일치하는 대댓글 내 author가 없을 시
+                            if reference == comment['authorDisplayName']: # 원댓글 author와 언급자가 동일한지
+                                reference_toWho = cur_index - reply_num # 동일하면 reference_toWho = 원댓글 index
+                            else:
+                                # 언급자 존재 X | 언급자 이름 내 띄쓰 존재
+                                reference_toWho = ""
                         else:
-                            reference_toWho = "대댓글 수 문제(언급자 존재 X)"
+                            close_reference_list = []
+                            for i in range(0, len(close_reference)):
+                                if reply_num < close_reference[i]: # 언급 대댓글이 target 대댓글보다 나중에 달렸을 시(최신순으로 읽어옴)
+                                    close_reference_list.append(abs(reply_num - close_reference[i])) # close_reference_list에 언급 대댓글과 target 대댓글 거리 차이 추가
+                                else:
+                                    close_reference_list.append(99999)
+                            if len(close_reference_list) == 0: # 언급 대댓글이 target 대댓글보다 먼저 달렸을 시 (아마 언급자가 먼저 달았던 댓글을 삭제한 경우)
+                                # 언급 대댓글 후 target 존재
+                                reference_toWho = ""
+                            else:
+                                close_index = min(close_reference_list) # close_reference_list 내에 가장 작은 값 = 언급 대댓글과 가장 가까운 값 추출
+                                reference_toWho = cur_index + close_index # 해당 index reference_toWho에 추가
+
+                        result_format = {
+                            "index": cur_index,
+                            "datatype": "2",
+                            "toWho": reference_toWho,
+                            "author": reply['snippet']['authorDisplayName'],
+                            "publishedDate": reply['snippet']['publishedAt'],
+                            "timeNum": re.sub(r'[^0-9]', '', reply['snippet']['publishedAt']),
+                            "text": reply['snippet']['textDisplay']
+                        }
+                        result.append(result_format)
+                        cur_index = cur_index + 1
+                        reply_num = reply_num + 1
+
+                    ### datatype "1" 대댓글
                     else:
-                        close_reference_list = []
-                        for i in range(0, len(close_reference)):
-                            if reply_num < close_reference[i]:
-                                close_reference_list.append((reply_num - close_reference[i]))
-                        if len(close_reference_list) == 0:
-                            reference_toWho = "대댓글 수 문제(언급 이후 동일인물 댓글 존재)"
-                        else:
-                            close_index = close_reference_list.index(min(close_reference_list))
-                            reference_toWho = cur_index + close_reference[close_index]
+                        result_format = {
+                            "index": cur_index,
+                            "datatype": "1",
+                            "toWho": cur_index - reply_num - 1,
+                            "author": reply['snippet']['authorDisplayName'],
+                            "publishedDate": reply['snippet']['publishedAt'],
+                            "timeNum": re.sub(r'[^0-9]', '', reply['snippet']['publishedAt']),
+                            "text": reply['snippet']['textDisplay']
+                        }
+                        result.append(result_format)
+                        cur_index = cur_index + 1
+                        reply_num = reply_num + 1
 
-                    result_format = {
-                        "datatype": "2",
-                        "toWho": reference_toWho,
-                        #"cid": reply['snippet']['authorChannelId']['value'],
-                        "author": reply['snippet']['authorDisplayName'],
-                        "published_date": reply['snippet']['publishedAt'],
-                        "time_num": re.sub(r'[^0-9]', '', reply['snippet']['publishedAt']),
-                        "text": reply['snippet']['textDisplay']
-                    }
-                    result[cur_index] = result_format
-                    cur_index = cur_index + 1
-                    reply_num = reply_num + 1
-
-                ### datatype "1" 대댓글
+                if 'nextPageToken' in request:
+                    request = api_obj.comments().list(part='snippet', parentId=parent, pageToken=request['nextPageToken'],
+                                                      maxResults=100).execute()
                 else:
-                    result_format = {
-                        "datatype": "1",
-                        "toWho": cur_index - reply_num - 1,
-                        #"cid": reply['snippet']['authorChannelId']['value'],
-                        "author": reply['snippet']['authorDisplayName'],
-                        "published_date": reply['snippet']['publishedAt'],
-                        "time_num": re.sub(r'[^0-9]', '', reply['snippet']['publishedAt']),
-                        "text": reply['snippet']['textDisplay']
-                    }
-                    result[cur_index] = result_format
-                    cur_index = cur_index + 1
-                    reply_num = reply_num + 1
+                    break
 
     if 'nextPageToken' in response:
         response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id,
                                                  pageToken=response['nextPageToken'], maxResults=100).execute()
     else:
         break
-
 
 ### 광고도배(중복), html 태그, 5글자 미만 댓글 삭제
 del_set = set()
@@ -117,12 +131,12 @@ for i in range(0, len(result)):
             del_set.add(i)
 
 for i in del_set:
-    result.pop(i)
-
+    for j in range(0, len(result)-1):
+        if result[j]['index'] == i:
+            result.pop(j)
 
 ### 시간순 정렬
-result = sorted(result.items(), key=lambda x: x[1]['time_num'], reverse=True)
-result = dict(result)
+result = sorted(result, key=lambda x: x['timeNum'], reverse=False)
 
 
 ######################## 감성분석 ##########################
@@ -145,11 +159,12 @@ tokenizer = ElectraTokenizerFast.from_pretrained(args['model_path'])
 ### 한 줄씩 감성 분석
 for i in result:
     try:
-        input_vector = tokenizer.encode(result[i]['text'], return_tensors='pt')
+        input_vector = tokenizer.encode(i['text'], return_tensors='pt')
         pred = model(input_ids=input_vector, labels=None).logits.argmax(dim=-1).tolist()
     except:
         continue
-    result[i]['score'] = pred[0]
+    i['score'] = pred[0]
 
-with open('dentist.json', 'w', encoding='utf-8') as make_file:
+### 파일 저장
+with open('frizia_1.json', 'w', encoding='utf-8') as make_file:
     json.dump(result, make_file, ensure_ascii=False, indent='\t')
